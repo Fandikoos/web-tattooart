f56f3e4b6af5c195f254ed51657d5e88cb676199
Fandikoos <danfandos@gmail.com>
2025-05-31 12:24:13 +0200

    Estructura proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FooterComponent } from "./core/footer/footer.component";
+import { HeaderComponent } from "./core/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+    FooterComponent,
+    HeaderComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
