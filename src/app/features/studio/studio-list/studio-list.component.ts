387fc64f56c412fe24b1bd42253fe54ad5ecd28f
Fandikoos <danfandos@gmail.com>
2025-09-02 19:40:07 +0200

    Añadiendo filtro por nombre para los estudios

--- src/app/features/studio/studio-list/studio-list.component.ts
+++ src/app/features/studio/studio-list/studio-list.component.ts
@@ -1,9 +1,12 @@
-import { Component, effect, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { StudioService } from '../../../shared/services/studio.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ButtonModule } from 'primeng/button';
 import { RouterLink } from '@angular/router';
 import { FilterComponent } from "../filter/filter.component";
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { Studio } from '../../../shared/models/Studio';
+import { PaginatorComponent } from '../../../shared/utils/paginator/paginator.component';
 
 
 @Component({
@@ -12,7 +15,9 @@ import { FilterComponent } from "../filter/filter.component";
   styleUrls: ['./studio-list.component.css'],
   imports: [
     RouterLink,
-    FilterComponent
+    FilterComponent,
+    MatPaginatorModule,
+    PaginatorComponent
 ]
 })
 export class StudioListComponent implements OnInit {
@@ -21,11 +26,23 @@ export class StudioListComponent implements OnInit {
 
   private studios$ = this.studioService.getAll();
   public studios = toSignal(this.studios$);
+  
+  // Señal para recibir los estudios paginados
+  paginatedStudios = signal<Studio[]>([]);
 
-  constructor() {}
-
-  ngOnInit() {
-
+  onPaginatedStudiosChange(studios: Studio[]){
+    // Se reciben los estudios ya paginados
+    this.paginatedStudios.set(studios);
   }
+
+  onFilterChange(name: string){
+    if(name.length > 0){
+      this.studioService.findByName(name).subscribe(data => this.paginatedStudios.set(data))
+    } else {
+      this.studioService.getAll().subscribe(data => this.paginatedStudios.set(data))
+    }
+  }
+
+  ngOnInit() {  }
 
 }
--- src/app/shared/services/studio.service.ts
+++ src/app/shared/services/studio.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Studio } from '../models/Studio';
 
 @Injectable({
@@ -20,5 +20,12 @@ export class StudioService {
     return this.httpClient.get<Studio>(`${this.urlApiStudio}/${idStudio}`)
   }
 
+  findByName(studioName: string): Observable<Studio[]>{
+    if(!studioName || studioName.trim().length < 2){
+      return of([]);
+    }
+    const params = new HttpParams().set('name', studioName.trim());
+    return this.httpClient.get<Studio[]>(`${this.urlApiStudio}/search`, { params });
+  }
 
 }
