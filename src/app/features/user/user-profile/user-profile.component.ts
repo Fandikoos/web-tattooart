a8a0a41886c76968497d1d08f3e0cb686f5f98a6
Fandikoos <danfandos@gmail.com>
2025-09-24 19:48:43 +0200

    Terminando estudios favoritos en perfil de usuario

--- src/app/features/user/user-profile/user-profile.component.ts
+++ src/app/features/user/user-profile/user-profile.component.ts
@@ -4,19 +4,24 @@ import { ProfileUserDto } from '../../../shared/models/interfaces/ProfileUserDto
 import { FavStudioService } from '../../../shared/services/fav-studio.service';
 import { StudioService } from '../../../shared/services/studio.service';
 import { Studio } from '../../../shared/models/interfaces/Studio';
+import { Router, RouterLink } from '@angular/router';
+import { Favourite } from '../../../shared/models/interfaces/Favourite';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
+  imports: [RouterLink]
 })
 export class UserProfileComponent implements OnInit {
 
   private tokenService = inject(TokenService);
   private favouriteService = inject(FavStudioService);
   private studioService = inject(StudioService);
+  private router = inject(Router);
   userProfile = signal<ProfileUserDto | null>(null);
   favStudiosByUser = signal<Studio[]>([]);
+  favourites: Favourite[] | undefined;
 
   constructor() { }
 
@@ -28,6 +33,7 @@ export class UserProfileComponent implements OnInit {
     this.favouriteService.getFavByIdUser(this.userProfile()!.idUser).subscribe(favs => {
       // Recogemos los ids de los estudios
       const idStudiosFavOfUser = favs.map(fav => fav.idStudio);
+      this.favourites = favs;
 
       if(idStudiosFavOfUser.length > 0){
         this.studioService.findByIdsStudios(idStudiosFavOfUser).subscribe(studios => {
@@ -36,5 +42,27 @@ export class UserProfileComponent implements OnInit {
       }
     })
   }
+
+  discardFav(studio: Studio) {
+    // Si es null, undefined hace sale del método sin ejectuar nada mas
+    if (!this.favourites) return;
+
+    // .find recorre el array hasta encontrar el primer elemento que cumple la condición y te devuelve el objeto
+    const fav = this.favourites.find(f => f.idStudio === studio.idStudio);
+    if (!fav) return;
+
+    this.favouriteService.deleteFav(fav.idFavourite).subscribe(() => {
+      // Quitar de la lista reactiva
+      this.favStudiosByUser.update(studios => 
+        studios.filter(s => s.idStudio !== studio.idStudio)
+      );
+      studio.isFav = false;
+      studio.idFavourite = null;
+
+      // Lo eliminamos de la lista local de favoritos tambien
+      this.favourites = this.favourites!.filter(f => f.idFavourite !== fav.idFavourite);
+    });
+  }
+
 
 }
